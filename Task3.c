ba263d9f582d4d7f38548bfcaed0790636785ef5
Keyonca <148034804+Keyonca@users.noreply.github.com>
2025-05-23 08:21:05 +0500

    Update Task3.c

--- Task3.c
+++ Task3.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include <time.h>
 
-#define N 200000
-
-void fill_arrays(double *a, double *b) {
+void fill_arrays(double *a, double *b, int N) {
     for (int i = 0; i < N; i++) {
         a[i] = i + 1.0;
         b[i] = (i + 1.0) * 2.0;
     }
 }
 
-void sequential_operations(double *a, double *b, double *sum, double *diff, double *prod, double *div) {
+void sequential_operations(double *a, double *b, double *sum, double *diff, double *prod, double *div, int N) {
     for (int i = 0; i < N; i++) {
         sum[i]  = a[i] + b[i];
         diff[i] = a[i] - b[i];
@@ -21,9 +18,7 @@ void sequential_operations(double *a, double *b, double *sum, double *diff, doub
     }
 }
 
-void parallel_operations(double *a, double *b, double *sum, double *diff, double *prod, double *div, int threads) {
-    omp_set_num_threads(threads);
-
+void parallel_operations(double *a, double *b, double *sum, double *diff, double *prod, double *div, int N) {
     #pragma omp parallel for
     for (int i = 0; i < N; i++) {
         sum[i]  = a[i] + b[i];
@@ -37,7 +32,18 @@ double get_time_sec() {
     return omp_get_wtime();
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    int N = 200000;
+
+    char *env_n = getenv("ARRAY_SIZE");
+    if (env_n != NULL) {
+        N = atoi(env_n);
+    }
+
+    if (argc > 1) {
+        N = atoi(argv[1]);
+    }
+
     double *a = malloc(N * sizeof(double));
     double *b = malloc(N * sizeof(double));
     double *sum  = malloc(N * sizeof(double));
@@ -45,23 +51,23 @@ int main() {
     double *prod = malloc(N * sizeof(double));
     double *div  = malloc(N * sizeof(double));
 
-    fill_arrays(a, b);
-
-    // Последовательный вариант
-    double t1 = get_time_sec();
-    sequential_operations(a, b, sum, diff, prod, div);
-    double t2 = get_time_sec();
-    printf("Sequential time: %f seconds\n", t2 - t1);
-
-    int thread_counts[] = {2, 4, 8, 16};
-
-    for (int i = 0; i < 4; i++) {
-        int threads = thread_counts[i];
-        double t_start = get_time_sec();
-        parallel_operations(a, b, sum, diff, prod, div, threads);
-        double t_end = get_time_sec();
-        printf("Parallel time with %d threads: %f seconds\n", threads, t_end - t_start);
+    if (!a || !b || !sum || !diff || !prod || !div) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
     }
+
+    fill_arrays(a, b, N);
+
+    double t1 = get_time_sec();
+    sequential_operations(a, b, sum, diff, prod, div, N);
+    double t2 = get_time_sec();
+    printf("Время выполнения последовательной программы: %f секунд\n", t2 - t1);
+
+    double t_start = get_time_sec();
+    parallel_operations(a, b, sum, diff, prod, div, N);
+    double t_end = get_time_sec();
+    int actual_threads = omp_get_max_threads();
+    printf("Время выполнения параллельной программы с %d потоками: %f секунд\n", actual_threads, t_end - t_start);
 
     free(a); free(b); free(sum); free(diff); free(prod); free(div);
     return 0;
